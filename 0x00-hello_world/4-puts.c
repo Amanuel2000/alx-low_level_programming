237fa97b379ced0e2bdc17b41a6447513b26fcbc
Amanuel2000 <amanuelf44@gmail.com>
2022-06-30 14:57:41 -0700

    changesa  added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
+#include <stdio.>
 /**
+ * main - prints the size of various types
  *
- * * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  *
- * * Return: 0 if exited properly, non-zero otherwise
- *
- * */
+ */
 int main(void)
 
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
