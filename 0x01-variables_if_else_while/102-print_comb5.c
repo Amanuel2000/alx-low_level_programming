f94429d52ecf2f96446e2b11e3444cf833926c7b
Amanuel2000 <amanuelf44@gmail.com>
2022-07-01 12:47:33 -0700

    adding changes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,38 +6,29 @@
 *Return: Always 0 (Success)
 */
 int main(void)
+
 {
-	int i, j, k, m;
-	i = 48;
-	while (i < 58)
+	int i, j;
+
+	for (i = 0; i < 100; i++)
 	{
-		j = 48;
-		while (j < 58)
+		for (j = 0; j < 100; j++)
 		{
-			m = j + 1;
-			k = i;
-			while (k < 58)
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-				while (m < 58)																	{
-						putchar(i);
-						putchar(j);
-						putchar(32);
-						putchar(k);
-						putchar(m);
-						if (i < 57 || j < 56 || k < 57 || m < 57)
-							{
-								putchar(44);
-								putchar(32)
-							}
-							m++;
-					}	
-					m = 48;
-					k++;
+					putchar(',');
+					putchar(' ');
 				}
-				j++;	
+			}
 		}
-		i++;
 	}
-	putchar(10);
-	return(0);
+putchar('\n');
+return (0);
 }
