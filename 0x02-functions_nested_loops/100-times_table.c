c436285390b30b5e1db29302e172a9a86e6c8381
Amanuel2000 <amanuelf44@gmail.com>
2022-07-05 15:45:27 -0700

    adding files

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,32 +6,35 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++){
+			for (j = 0; j <= n; j++)
 			{
-			       	k = j * i;
+				k = j * i;
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
+				}
+			       	else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
+				}
+			       	else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k / 10) + '0');
 					_putchar((k % 10) + '0');
-				} else if (k >= 100)
+				}
+			       	else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(',');
@@ -40,7 +43,7 @@ void print_times_table(int n)
 					_putchar((k % 10) + '0');
 				}
 			}					
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
