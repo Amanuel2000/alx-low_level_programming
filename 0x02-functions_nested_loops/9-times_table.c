18c1cbfb763746ab8a370e24b429c9b1a9c242cd
Amanuel2000 <amanuelf44@gmail.com>
2022-07-05 13:45:15 -0700

    hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,20 +13,20 @@ void times_table(void)
 			k = x * y;
 			if (y == 0)
 				_putchar(k + '0');
-				if (y != 0 && k < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-												}
+			if (y != 0 && k < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}												}
 		_putchar('\n');
 	}
 }
