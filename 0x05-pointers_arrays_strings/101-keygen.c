d671490e644343fb6e65ce66d608a9756a316ea5
Amanuel2000 <amanuelf44@gmail.com>
2022-07-11 13:13:13 -0700

    hot fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,66 +1,63 @@
 #include <math.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
 
-/**
-*Function to randomly generates password
-*of length N
-*/
-void randomPasswordGeneration(int N)
-{
-	int i = 0;
-	int randomizer = 0;
-	
-	/**
-	*Seed the random-number generator
-	* with current time so that the
-	*numbers will be different every time
-	*/
-	srand((unsigned int)(time(NULL)));
-	char numbers[] = "0123456789";
-	char letter[] = "abcdefghijklmnoqprstuvwyzx";
-	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-	char symbols[] = "!@#$^&*?";
-	char password[N];
-	randomizer = rand() % 4;
+#include "main.h"
 
-	for (i = 0; i < N; i++) 
-	{
-		if (randomizer == 1) {
-			password[i] = numbers[rand() % 10];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-	}
-		else if (randomizer == 2) 
-		{
-			password[i] = symbols[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 3) 
-		{
-			password[i] = LETTER[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else {
-			password[i] = letter[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-	}
-}
+
+
+/**
+
+*generationRandomPassword - Randomly generate passwords.
+
+*@N: password length.
+
+*Decription: Function to randomly generates password.
+
+*Return: nothing
+
+*/
+
+
 
 int main()
+
 {
-	/** 
-	*Length of the password to
-	*be generated
-	*/
-	int N = 10;
 
-	randomPasswordGeneration(N);
+	generationRandomPassword(15);
 
-	return(0);
+	return (0);
+
+}
+
+
+
+void generationRandomPassword(int N)
+
+{
+
+	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/|";
+
+
+
+	int i;
+
+	
+
+	srand(time(NULL));
+
+
+
+	for (i = 0; i < N; i++)
+
+	{
+
+		printf("%c", list[rand() % (sizeof(list) - 1)]);
+
+	}
+
 }
