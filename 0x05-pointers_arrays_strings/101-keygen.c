f9d8fd2b0a88b8236fadb66857a780627346531d
Amanuel2000 <amanuelf44@gmail.com>
2022-07-11 14:04:11 -0700

    hot fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,41 +4,26 @@
 #include <time.h>
 #include "main.h"
 
-
 /**
-
 *generationRandomPassword - Randomly generate passwords.
-
 *@N: password length.
-
 *Decription: Function to randomly generates password.
-
 *Return: nothing
-
 */
-
 int main(void)
-
 {
 	generationRandomPassword(15);
-
 	return (0);
-
 }
-
-
-
 void generationRandomPassword(int N)
-
 {
 	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/|";
 
 	int i;
+
 	srand(time(NULL));
 	for (i = 0; i < N; i++)
-
 	{
 		printf("%c", list[rand() % (sizeof(list) - 1)]);
-
 	}
 }
