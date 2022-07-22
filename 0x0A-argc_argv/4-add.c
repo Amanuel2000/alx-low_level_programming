80beea88d744f063e2ff3e8aade42992ca16fecd
Amanuel2000 <amanuelf44@gmail.com>
2022-07-22 14:31:31 -0700

    hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
-* num_checker - checks if a given char is number or not
-* @a: char to be checked
-* Return: 1, if its a number, else 0
+*num_checker - checks if a given char is number or not
+*@a: char to be checked
+*Return: 1, if its a number, else 0
+
 **/
 
 int num_checker(char *a)
@@ -46,16 +48,11 @@ int num_checker(char *a)
 }
 
 /**
-
- * main - add positive numbers
-
- * @argc: arguement count
-
- * @argv: array of pointers to arguement strings
-
- * Return: result of addition or 1
-
- **/
+* main - add positive numbers
+* @argc: arguement count
+* @argv: array of pointers to arguement strings
+* Return: result of addition or 1
+**/
 
 int main(int argc, char *argv[])
 
