5664dfda6bb0d73a4cf0667fb82138b860923572
Amanuel2000 <amanuelf44@gmail.com>
2022-08-04 12:00:56 -0700

    More than 40 lines in a function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-
 #include <stdarg.h>
-
 #include "variadic_functions.h"
-
-
-
 /**
 * print_all - print char, integer, float and string
 * @format: format
 */
-
 void print_all(const char * const format, ...)
 {
 	va_list list;
-
 	unsigned int j = 0, start = 0;
-
 	char *p;
 
 	va_start(list, format);
