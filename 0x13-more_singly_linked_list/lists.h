374e20c07864912db623336d92b8636ab07e6bd0
Amanuel2000 <amanuelf44@gmail.com>
2022-08-15 23:19:32 -0700

    header file

--- 0x13-more_singly_linked_list/lists.h
+++ 0x13-more_singly_linked_list/lists.h
@@ -1,16 +1,16 @@
-#ifndef _LISTS_
+#ifndef LISTS_H
 
-#define _LISTS_
+#define LISTS_H
+
+#include <string.h>
 
 #include <stdio.h>
-
-#include <stdlib.h>
-
 
 /**
 * struct listint_s - singly linked list
 * @n: integer
 * @next: points to the next node
+*
 * Description: singly linked list node structure
 * for Holberton project
 */
@@ -24,25 +24,6 @@ typedef struct listint_s
 	struct listint_s *next;
 
 } listint_t;
-
-
-
-/**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
- *
- * Description: singly linked list of pointers
- */
-typedef struct listp_s
-
-{
-
-	void *p;
-
-	struct listp_s *next;
-
-} listp_t;
 
 
 
@@ -75,7 +56,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 
 listint_t *find_listint_loop(listint_t *head);
-
-
 
 #endif
